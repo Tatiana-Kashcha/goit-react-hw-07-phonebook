1bb7af3a78da749744ba8c74d8ad5f07d7969a2c
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-21 00:51:42 +0300

    dell persistor

--- src/redux/contactsSlise.js
+++ src/redux/contactsSlise.js
@@ -1,7 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 export const contactsSlice = createSlice({
   name: 'contacts',
@@ -31,14 +29,6 @@ export const contactsSlice = createSlice({
   },
 });
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-export const contactsPersistReducer = persistReducer(
-  persistConfig,
-  contactsSlice.reducer
-);
-
 export const { addUser, deleteUser } = contactsSlice.actions;
+
+export const contactsReducer = contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { contactsPersistReducer } from './contactsSlise';
+
+import { contactsReducer } from './contactsSlise';
 import { filterReducer } from './filterSlise';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
-    contactsUser: contactsPersistReducer,
+    contactsUser: contactsReducer,
     filterUser: filterReducer,
   },
-  middleware(getDefaultMiddleware) {
-    return getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    });
-  },
 });
-
-export const persistor = persistStore(store);
