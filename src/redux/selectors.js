bb357b0d2cb212d9aef4967ebdda61bf938d447b
Tatiana Kashcha <tkashcha@gmail.com>
2023-07-22 01:15:36 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,13 @@
 import { createSelector } from '@reduxjs/toolkit';
 
 export const selectUsers = state => state.contactsUser.contacts.items;
+
 export const selectFilter = state => state.filterUser.filter;
+
+export const selectIsLoading = state => state.contactsUser.contacts.isLoading;
+
+export const selectError = state => state.contactsUser.contacts.error;
+
 export const selectUsersQuantity = state =>
   state.contactsUser.contacts.items.length;
 
